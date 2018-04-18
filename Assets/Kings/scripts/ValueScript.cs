3b6aae4b78452429fae33bc6c7465a74737e7a99
Dal <purpledal@gmail.com>
2018-04-18 17:40:05 +0900

    소스 분석.

--- Assets/Kings/scripts/ValueScript.cs
+++ Assets/Kings/scripts/ValueScript.cs
@@ -6,7 +6,8 @@ using UnityEngine.Events;
 
 
 /// <summary>
-/// todo. 스크립트를 연결하기 위한 빈 오브젝트들.  
+/// 게임에서 사용되는 수치들을 계산하고 여러가지 처리등을 하기 위한 클래스.
+/// 이 스크립트는 게임씬내에 빈 오브젝트들을 만들어서 연결해놓는다.  
 /// Game -> Values -> adreadyValue
 /// Game -> Values -> Army
 /// Game -> Values -> AuthorityValue
@@ -31,19 +32,23 @@ using UnityEngine.Events;
 public class ValueScript : MonoBehaviour {
 
     /// <summary>
-    /// 현재 내가 붙어 있는 게임오브젝트가 게임에서 어떤 값의 유형인지 인스펙터 드롭다운 목록에서 선택해서 지정한다.
-    /// 가령 내가 붙어 있는 게임오브젝트가 Game -> Values -> Name 이면 인스펙터 드롭다운 메뉴에서 Name를 선택한다.
+    /// 현재 내가 붙어 있는 게임오브젝트가 게임에서 어떤 것을 담당하는지 유형인지 인스펙터 드롭다운 목록에서 선택해서 지정한다.
+    /// 가령 내가 군대수치와 관련된 작동들을 할려고 하면, 게임씬에 빈 오브젝트로 만들어서 이름을 Army로 짓고, 이 클래스를 연결한다.
+    /// 즉 Game -> Values -> Army이면 인스펙터 드롭다운 메뉴에서 Army를 선택해주어서 내가 붙어 있는 오브젝트의 성격을 규정해준다.
     /// </summary>
 	[Tooltip("내가 붙어 있는 게임오브젝트가 어떤 유형인지 옆에 드롭박스 목록에서 선택하세요.")]
 	public valueDefinitions.values valueType;
 
     /// <summary>
-    /// 사용자가 인스펙터에서 내가 붙어 있는 객체가 무엇인지(이름, 성별 등등)지정한 값을 문자열로 저장하고 있는 변수.
+    /// 플레이어프랩스에 키값으로 사용되는 텍스트를 가지고 있는 변수.
+    /// 사용자가 인스펙터에서 지정한 내가 붙어 있는 객체가 무엇인지(이름, 성별, 군인수 등등)지정한 값을 문자열로 저장하고 있는 변수.
+    /// 가령 군인수치를 담당하는 객체이면 프로그래머가 valueType 변수에 'Army'열거요소로 값을 할당했을 것이다. 
+    /// 이 값을 본 변수에 문자(텍스트)로 저장하고 있으며, 이 값을 플레이어프랩스에 키값으로 사용하기 위해 따로 변수를 만들어둔것이다.
     /// </summary>
 	private string identifier = "valuename";
 
     /// <summary>
-    /// ????? 무슨값이지????
+    /// 플레이어프랩스에 저장되어 있는 내가 붙어 있는 객체가 무엇인지(이름, 성별등등)을 가져와서 저장하는 변수
     /// </summary>
 	[ReadOnlyInspector] /// 변수를 인스펙터에서 읽기 전용으로 만들기위한 프로그래머가 만든 애트리뷰트.
     public float value = 0f;
@@ -55,7 +60,8 @@ public class ValueScript : MonoBehaviour {
 
 	void Awake()
     {
-        /// 오브젝트의 성격. 프로그래머가 인스펙터에서 내가 붙어 있는 객체를 무엇으로 활용할것인지 지정한 값을 문자로 저장한다. (이름, 성별, 나라, 돈 등등)
+        /// 프로그래머가 인스펙터에서 내가 붙어 있는 객체가 무엇인지 지정한 값을 문자로 저장한다. (이름, 성별, 나라, 돈 등등)
+        /// 가령 인스펙터에서 내가 붙어 있는 객체의 역할이 군인수치를 관리하는 객체라면 'Army'로 지정했을 것이고, 이때 식별자로 사용하는 변수에 'Army'텍스트 문자를 따로 저장한다.
 		identifier = valueType.ToString ();
 
 		loadValue();
@@ -114,35 +120,72 @@ public class ValueScript : MonoBehaviour {
 	}
 
 
+    /// <summary>
+    /// 최소값과 최대값의 범위를 지정한 클래스. 
+    /// 여기서는 일단 대충 기본값으로 초기화하고, 
+    /// 실제로 부여하는 값은 인스펙터에서 노출된 변수에서 프로그래머가 해당 오브젝트의 성격에 따라 최소값과 최대값을 지정한다.
+    /// </summary>
 	[System.Serializable]
 	public class valueLimits
 	{
-		[Tooltip("What is the minimum possible value?")]
+        /// <summary>
+        /// 프로그래머가 설정하는 밸류값이 벗어나면 안되는 범위의 최소값
+        /// </summary>
+		[Tooltip("가능한 최소값은 무엇입니까?")]
 		public float min = 0f;
-		[Tooltip("What is the maximum possible value?")]
+
+        /// <summary>
+        /// 프로그래머가 설정하는 밸류값이 벗어나면 안되는 범위의 최대값
+        /// </summary>
+		[Tooltip("가능한 최대값은 무엇입니까?")]
 		public float max = 100f;
-		[Tooltip("For initialization with random values: What is the minimum random value.")]
+
+        /// <summary>
+        /// 랜덤으로 값을 뽑기할때 랜덤범위의 최소값.
+        /// </summary>
+		[Tooltip("랜덤으로 값을 할당하는 경우 허용 가능한 최소값을 입력하세요.")]
 		public float randomMin = 0f;
-		[Tooltip("For initialization with random values: What is the maximum random value.")]
+
+        /// <summary>
+        /// 랜점으로 값을 뽑기할때 랜덤범위의 최대값.
+        /// </summary>
+		[Tooltip("랜덤으로 값을 할당하는 경우 허용 가능한 최대값을 입력하세요.")]
 		public float randomMax = 100f;
 	}
 
+    /// <summary>
+    /// 최소값과 최대값의 범위. 내가 붙어 있는 오브젝트의 성격에 따라 인스펙터에서 각각의 값의 범위를 지정한다.
+    /// </summary>
 	public valueLimits limits;
 
-	public void limitValue(){
-		if (value < limits.min) {
+    /// <summary>
+    /// 밸류
+    /// </summary>
+	public void limitValue()
+    {
+        /// 만약 밸류값이 내가 설정한 범위보다 작은 경우
+		if (value < limits.min)
+        {
+            /// 밸류값에 내가 설정한 범위 최소값을 할당한다.
 			value = limits.min;
+            /// 최소값일때 작동할 유니티이벤트를 실행한다.
 			events.OnMin.Invoke();
 		}
-		if (value > limits.max) {
+        /// 만약 밸류값이 내가 설정한 범위보다 큰 경우
+		if (value > limits.max)
+        {
+            /// 밸류값에 내가 설정한 범위 최대값을 할당한다.
 			value = limits.max;
+            /// 최대값일때 작동할 유니티이벤트를 실행한다.
 			events.OnMax.Invoke();
 		}
 	}
 
-	/*
-	 * Increas or decrease a value. 
-	 */
+	/// <summary>
+    /// 값을 늘리거나 줄인다.
+    /// </summary>
+    /// <param name="increment"></param>
+    /// <returns></returns>
 	public float addValue (float increment){
 
 		if (debugValueChanges == true) {
@@ -187,9 +230,11 @@ public class ValueScript : MonoBehaviour {
     /// <returns></returns>
     public float setValueRandom()
     {
-			value = Random.Range (limits.randomMin, limits.randomMax);
-			limitValue ();
-			saveValue ();
+        /// 랜덤으로 지정한 값을 배정하고
+        value = Random.Range (limits.randomMin, limits.randomMax);
+
+        limitValue ();
+		saveValue ();
 		return value;
 	}
 
@@ -220,6 +265,7 @@ public class ValueScript : MonoBehaviour {
         /// 현재 내가 붙어 있는 오브젝트이 성격을 지정한 값이 기존 플레이어프랩스에 이미 저장되어 있는 값인지 검사해서, 이미 저장되 있는 키값일 경우 
 		if (SecurePlayerPrefs.HasKey (identifier))
         {
+            /// 플레이어프랩스에 저장되어 있는 키값을 가져온다.
 			value = SecurePlayerPrefs.GetFloat (identifier);
 		}
         else /// 저장되 있는 키값이 아닌 경우에는
@@ -262,17 +308,43 @@ public class ValueScript : MonoBehaviour {
 		saveValue ();
 	}
 
-	[System.Serializable] public class mEvent : UnityEvent {}
+    /// <summary>
+    /// 유니티 이벤트.
+    /// </summary>
+	[System.Serializable] /// 유니티 이벤트를 인스펙터에 노출시키기 위해 직렬화해준다.
+    public class mEvent : UnityEvent {}
 
-	[System.Serializable]
-	public class valueEvents{
+    /// <summary>
+    /// 유니티 이벤트들의 모음 클래스.
+    /// </summary>
+	[System.Serializable] /// 클래스를 인스펙터에 노출시키기 위해 직렬화해준다.
+	public class valueEvents
+    {
+        /// <summary>
+        /// 증가할때 실행할 유니티이벤트를 연결하는 변수
+        /// </summary>
 		public mEvent OnIncrease;
-		public mEvent OnDecrease;
+
+        /// <summary>
+        /// 감소할때 실행할 유니티이벤트를 연결하는 변수
+        /// </summary>
+        public mEvent OnDecrease;
+
+        /// <summary>
+        ///  최대일때 실행할 유니티이벤트를 연결하는 변수
+        /// </summary>
 		public mEvent OnMax;
+
+        /// <summary>
+        /// 최소일때 실행할 유니티이벤트를 연결하는 변수
+        /// </summary>
 		public mEvent OnMin;
 	}
 
-	[Tooltip("Events are triggered on value changes or if the value is at one of its limits.")]
+    /// <summary>
+    /// 인스펙터에서 유니티이벤트를 사용하기 위해 노출하는 변수.
+    /// </summary>
+	[Tooltip("이벤트는 값 변경시 또는 값이 한계 중 하나에 있을때 트리거됩니다.")]
 	public valueEvents events;
 
 
--- Assets/Kings/scripts/valueDefinitions.cs
+++ Assets/Kings/scripts/valueDefinitions.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class valueDefinitions : MonoBehaviour {
     
     /// <summary>
-    /// 게임의 모든 가치를 정의한 열거형. 이름, 성별, 나라, 카리스마등 게임에서 사용되는 모든 종류의 분류를 정의해놓은 열거형이다. 
+    /// 게임에서 사용되는 모든 가치를 정의한 열거형. 이름, 성별, 나라, 카리스마등 게임에서 사용되는 모든 종류의 분류를 정의해놓은 열거형이다. 
     /// 유니티에서 열거형 변수는 인스펙터에서 드롭 다운 메뉴를 표시되기때문에 사용자가 열거형에 요소를 쉽게 선택해서 지정할 수 있다.
     /// 가령 이름 변수일경우 인스펙터 드롭다운 메뉴에서 name을 지정하는 방식으로 사용한다.
     /// </summary>
     public enum values
     {
-        name,
-        surname,
-        gender,
-        country,
-        years,
-        army,
-        people,
-        religion,
-        money,
-        authority,
-        intelligence,
-        charisma,
-        luck,
-        creativity,
-        look,
-        health,
-        marriage,
-        married,
-        adready,
-        gamesPlayed
+        name, // 이름
+        surname, // 성
+        gender, // 성별
+        country, // 나라
+        years, // 통치기간
+        army, // 군대수치
+        people, // 국민수치
+        religion, // 종교수치
+        money, // 돈
+        authority, // 권위
+        intelligence, // 지성
+        charisma, // 카리스마
+        luck, // 행운
+        creativity, // 독창성
+        look, //
+        health, // 건강
+        marriage, // 결혼
+        married, // 기혼
+        adready, // ??? 이미있다???
+        gamesPlayed // 게임플레이중
     }
 }
